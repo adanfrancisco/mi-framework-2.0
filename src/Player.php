916753365279e5d1f92dc68c071ee745c70fc114
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-04-16 09:44:17 -0300

    Entrar

--- src/Player.php
+++ src/Player.php
@@ -7,12 +7,21 @@ class Player
     private $key = 'X';
     private $name = '';
     private $mano = [];
+    private $truco;
+    private $envido;
+    private $valorManoEnvido;
+
+    const MAX_VALUE_tRUCO = 39 + 38 + 37;
+
 
     public function __construct(string $name, int $id = null, string $key = null )
     {
         $this->id = $id;
         $this->key = $key;
         $this->name = $name;
+        $this->truco = [];
+        $this->cantarEnvido = [];
+        $this->valorManoTruco();
     }
 
     public function getKey()
@@ -45,6 +54,70 @@ class Player
         }
         return $respuesta;
     }
-    
+
+    public function valorManoTruco(){
+        $score = 0;
+        foreach ($this->mano as $carta) {
+            $score += $carta->getGameValue();
+        }
+        if(self::MAX_VALUE_TRUCO * (6/7) < $score){
+            // si truco
+            $this->truco['truco'] = 1;
+            // si retruco
+            $this->truco['retruco'] = 1;
+            // si vale cuatro
+            $this->truco['vale-cuatro'] = 1;
+
+        }elseif(self::MAX_VALUE_TRUCO * (5/7) < $score)  {
+            // si truco
+            $this->truco['truco'] = 1;
+            // si retruco
+            $this->truco['retruco'] = 1;
+            // no vale cuatro
+            $this->truco['vale-cuatro'] = 0;
+        }elseif(self::MAX_VALUE_TRUCO * (4/7) < $score){
+            // si truco
+            $this->truco['truco'] = 1;
+            // no retruco
+            $this->truco['retruco'] = 0;
+            // no vale cuatro
+            $this->truco['vale-cuatro'] = 0;
+        }elseif(self::MAX_VALUE_TRUCO * (3/7) < $score){
+            // si truco
+            $this->truco['truco'] = 1;
+            // no retruco
+            $this->truco['retruco'] = 0;
+            // no vale cuatro
+            $this->truco['vale-cuatro'] = 0;
+        }else{
+            // no truco
+            $this->truco['truco'] = 0;
+            // no retruco
+            $this->truco['retruco'] = 0;
+            // no vale cuatro
+            $this->truco['vale-cuatro'] = 0;
+        }
+    }
+
+    public function cantarTruco(string $queCantar) : boolean{
+        return $this->truco[$queCantar];
+    }
+    public function valorManoEnvido(){
+        $manoParaElEnvido = [];
+        foreach ($this->mano as $carta) {
+            $manoParaElEnvido[$carta->getType()][] = $carta;
+        }
+        $countManoMaxima = 0;
+        $k = -1;
+        foreach ($manoParaElEnvido as $key => $mano) {
+            if(  $countManoMaxima < count($mano) ){
+                $countManoMaxima = count($mano);
+                $k = $key;
+            }
+        }
+        $manoParaElEnvido = $manoParaElEnvido[$key];
+    }
+    public function cantarEnvido(){}
+
     
 }
